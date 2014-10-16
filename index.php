68d39aa87ecf1c5014978a59fa765ec5705b3778
wardwar <wildan.aga@gmail.com>
2014-10-17 01:11:42 +0700

    upload file

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Wildan Angga Rahman</title>
+
+    <!-- Bootstrap -->
+    <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+
+    
+
+  </head>
+  <body>
+  <!-- section head home -->
+    <section id="home" data-type="background" data-speed="10" class="pages"></section>
+  <!-- section about -->
+    <section id="about" data-type="background" data-speed="10" class="pages"></section>
+  <!-- section resume -->
+    <section id="resume" data-type="background" data-speed="10" class="pages"></section>
+  <!-- section skill -->
+    <section id="skills" data-type="background" data-speed="10" class="pages"></section>
+  <!-- section works -->
+    <section id="works" data-type="background" data-speed="10" class="pages"></section>
+  <!-- section testimonials -->
+    <section id="testimonials" data-type="background" data-speed="10" class="pages"></section>
+  <!-- section contact -->
+    <section id="contact" data-type="background" data-speed="10" class="pages"></section>
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/script.js"></script>
+    
+  </body>
+</html>
